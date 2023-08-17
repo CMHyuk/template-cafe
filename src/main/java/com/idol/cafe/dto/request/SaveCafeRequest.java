34cc79169f2434e592f985e34fbbea1c30c5a21c
CMHyuk <alsgur9710@naver.com>
2023-08-17 15:22:18 +0900

    template

--- src/main/java/com/idol/cafe/dto/request/SaveCafeRequest.java
+++ src/main/java/com/idol/cafe/dto/request/SaveCafeRequest.java
@@ -2,11 +2,12 @@ package com.idol.cafe.dto.request;
 
 import com.idol.cafe.domain.Address;
 import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 public class SaveCafeRequest {
     private String cafeName;
     private String introduction;
-    private String imageUrl;
+    private MultipartFile file;
     private Address address;
 }
